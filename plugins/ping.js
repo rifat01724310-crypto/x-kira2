90e259090a263feff01b05e38dfbaf2be9d81cef
ali-sain <queensapna26@gmail.com>
2025-12-21 18:36:53 +0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -3,106 +3,27 @@ const { Module } = require("../lib/plugins");
 Module({
   command: "ping",
   package: "mics",
-  description: "Replies with the bot latency",
+  description: "Replies with bot latency",
 })(async (message) => {
   const start = Date.now();
   const userName = message.pushName || "User";
-  let gift = {
-      key: {
-        fromMe: false,
-        participant: "0@s.whatsapp.net",
-        remoteJid: "status@broadcast"
-      },
-      message: {
-        contactMessage: {
-          displayName: `${userName}`,
-          vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:'STARK-MD'\nitem1.TEL;waid=${m.sender.split("@")[0]}:${m.sender.split("@")[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-        }
-      }
-    };
-  const emojis = [
-    "⛅",
-    "👻",
-    "⛄",
-    "👀",
-    "🪁",
-    "🪃",
-    "🎳",
-    "🎀",
-    "🌸",
-    "",
-    "🍥",
-    "🎀",
-    "🍓",
-    "🍡",
-    "💗",
-    "🦋",
-    "💫",
-    "💀",
-    "☁️",
-    "🌨️",
-    "🌧️",
-    "🌦️",
-    "🌥️",
-    "⛅",
-    "🪹",
-    "⚡",
-    "🌟",
-    "☁️",
-    "🎐",
-    "🏖️",
-    "🎐",
-    "🪺",
-    "🌊",
-    "🐚",
-    "🪸",
-    "🍒",
-    "🍇",
-    "🍉",
-    "🌻",
-    "🎢",
-    "🚀",
-    "🍫",
-    "💎",
-    "🌋",
-    "🏔️",
-    "⛰️",
-    "🌙",
-    "🪐",
-    "🌲",
-    "🍃",
-    "🍂",
-    "🍁",
-    "🪵",
-    "🍄",
-    "🌿",
-    "🐞",
-    "🐍",
-    "🕊️",
-    "🕷️",
-    "🕸️",
-    "🎃",
-    "🏟️",
-    "🎡",
-    "🥂",
-    "🗿",
-    "⛩️",
-  ];
+
+  // Random emoji
+  const emojis = ["⛅","👻","⛄","👀","🪁","🎳","🌸","🍓","💗","🦋","💀","☁️","⚡","🌟","🎐","🏖️","🌊","🐚","🍇","🍉","🌻","🚀","🍫","💎","🌋","🏔️","🌙","🪐","🌲","🍂","🍁","🐞","🕊️","🎃","🎡","🥂","⛩️"];
   const emoji = emojis[Math.floor(Math.random() * emojis.length)];
   await message.react(emoji);
+
   const latency = Date.now() - start;
-  //await message.send(`*${emoji}⧫𝔓⦿𝖓𝖌 ${latency} 𝖒ˢ*`, { edit: sent.key });
+
+  // Fancy quoted message
+  const gift = {
+    key: { fromMe: false, participant: "0@s.whatsapp.net", remoteJid: "status@broadcast" },
+    message: { contactMessage: { displayName: userName } }
+  };
 
   await message.conn.sendMessage(
     message.from,
-    {
-      text: `*${emoji} 𝐏๏፝֟ƞ̽g: ${latency} 𝐌sᷱ᪳*`,
-      contextInfo: {
-        mentionedJid: [message.sender],
-        forwardingScore: 5,
-        isForwarded: false,
-      },
-    },
+    { text: `*${emoji} 𝐏๏፝֟ƞ̽g: ${latency} 𝐌sᷱ᪳*`, contextInfo: { mentionedJid: [message.sender] } },
     { quoted: gift }
   );
 });
